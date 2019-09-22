6a5d22202756ee828e8425be6109dff0c8b91bcd
basheer <basheer@DESKTOP-0P35L7C>
2019-09-22 21:34:39 +0530

    Package 2

---
+++ My Java Project/src/com/package2/D.java
@@ -0,0 +1,11 @@
+//Accessing non-static variables in different package
+package com.package2;
+import com.package1.C;;
+public class D {
+
+	public static void main(String[] args) {
+		C ob=new C();
+
+	}
+
+}
